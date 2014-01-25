dded6e32162de74fb6a1a40ef638a20cccd1926a
Ryan Nielson <ryan@toplog.ca>
2014-01-25 13:48:32 -0400

    Added player animation transitions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,25 @@ public class Player : MonoBehaviour
 {
 	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
 
-	//Animator anim;										// Reference to the player's animator component.
+	Animator animator;									// Reference to the player's animator component.
+
+	float rotationAngle = 0f;
 
 	void Awake()
 	{
-		//anim = GetComponent<Animator>();
+		animator = GetComponent<Animator>();
 	}
 	
 	
 	void FixedUpdate()
 	{
 		// Set the vertical animation
-		//anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+
 
 		InputDevice device = InputManager.ActiveDevice;
-		Debug.Log (device.Direction.x);
-		Debug.Log (device.Direction.y);
+		//Debug.Log (device.Direction.x);
+		//Debug.Log (device.Direction.y);
 
 		//Debug.Log (device.Direction.sqrMagnitude);
 
@@ -28,11 +31,23 @@ public class Player : MonoBehaviour
 		//	transform.rotation = Quaternion.LookRotation(device.Direction, Vector2.up);
 		//}
 		//Debug.Log (Mathf.Atan2(device.Direction.y, device.Direction.x));
-		float rotationAngle = (Mathf.Atan2 (device.Direction.y, device.Direction.x) * Mathf.Rad2Deg);
-		Debug.Log (rotationAngle);
-		Debug.Log ("------");
+		//float rotationAngle = (Mathf.Atan2 (device.Direction.y, device.Direction.x) * Mathf.Rad2Deg);
+		//transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, rotationAngle));
+		//Debug.Log (rotationAngle);
+		//Debug.Log ("------");
 		// Pass all parameters to the character control script.
 		Move(device.Direction);
+
+		Debug.Log (rigidbody2D.velocity.sqrMagnitude);
+		animator.SetFloat("Speed", rigidbody2D.velocity.sqrMagnitude);
+	}
+
+	public void Update() {
+		InputDevice device = InputManager.ActiveDevice;
+		if (device.Direction != Vector2.zero)
+			rotationAngle = (Mathf.Atan2 (device.Direction.y, device.Direction.x) * Mathf.Rad2Deg);
+
+		transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, rotationAngle));
 	}
 	
 	
