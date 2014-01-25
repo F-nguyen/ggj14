99506e4e35e5c4590b30fcf8a699799c8cba606d
Ryan Nielson <ryan@toplog.ca>
2014-01-25 12:30:56 -0400

    YAA

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform target;
+	public float damping = 1;
+	
+	float offsetZ;
+	Vector3 lastTargetPosition;
+	Vector3 currentVelocity;
+	
+	// Use this for initialization
+	void Start () {
+		//lastTargetPosition = target.position;
+		offsetZ = (transform.position - target.position).z;
+		transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.SmoothDamp(transform.position, target.position + Vector3.forward * offsetZ, ref currentVelocity, damping);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using InControl;
+
+public class Player : MonoBehaviour 
+{
+	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
+
+	//Animator anim;										// Reference to the player's animator component.
+
+	void Awake()
+	{
+		//anim = GetComponent<Animator>();
+	}
+	
+	
+	void FixedUpdate()
+	{
+		// Set the vertical animation
+		//anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+		// Read the inputs.
+		//bool crouch = Input.GetKey(KeyCode.LeftControl);
+		//float h = CrossPlatformInput.GetAxis("Horizontal");
+		InputDevice device = InputManager.ActiveDevice;
+		Debug.Log (device);
+		// Pass all parameters to the character control script.
+		//Move(h);
+	}
+	
+	
+	public void Move(float move)
+	{
+		// The Speed animator parameter is set to the absolute value of the horizontal input.
+		//anim.SetFloat("Speed", Mathf.Abs(move));
+		
+		// Move the character
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+	}
+}
